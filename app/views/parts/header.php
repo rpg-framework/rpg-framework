ff8125d64edb4abf62179b308f96775ac4681ff7
fuatboluk <mektup@fuatboluk.com.tr>
2025-01-12 09:56:13 +0300

    Create header.php

---
+++ app/views/parts/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php echo $title; ?></title>
+
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/style.css">
+
+</head>
