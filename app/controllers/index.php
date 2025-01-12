78de6f36cb58b7e9466be941fb1fa6b34a497e9a
fuatboluk <mektup@fuatboluk.com.tr>
2025-01-12 10:00:16 +0300

    Update index.php

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -5,7 +5,16 @@ class index extends controller
 
     public function run()
     {
-        echo "Welcome to the Home Page";
+        //echo "Welcome to the Home Page";
+
+        $data["title"] = "Welcome to the Home Page";
+        $data["base_url"] = settings::$scheme."://".settings::$host;
+
+        // PHP
+        //$this->render("parts/header", $data);
+
+        // HTML
+        $this->view("parts/header", $data);
     }
 
 }
