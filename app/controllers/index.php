d5cea0d966ee0abb1983dab6d97899092244e3eb
Fuat Bölük <mektup@fuatboluk.com.tr>
2025-01-22 08:20:06 +0300

    New welcome page created

---
+++ app/controllers/index.php
@@ -0,0 +1,22 @@
+<?php
+
+class index extends controller
+{
+
+    public function main()
+    {
+        $data = new stdClass;
+
+        $data->title = "RPG Framework";
+        $data->background = "/assets/img/rpg-v1-background.jpg";
+
+        $data->application = "rpg";
+        $data->version = "1.0";
+
+        $data->phpver = phpversion();
+        $data->software = $_SERVER["SERVER_SOFTWARE"];
+
+        $this->view("index", $data);
+    }
+
+}
